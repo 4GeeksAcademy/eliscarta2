642d240e964eb2026b57ae4fefef2dada49f53c7
ElisG24 <160186837+ElisG24@users.noreply.github.com>
2024-07-20 15:17:58 +0000

    trabajofinalizado

--- src/app.js
+++ src/app.js
@@ -6,6 +6,35 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const myEmojis = ["♠️", "♣️", "♦️", "♥"];
+  const myNumbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  const randomNumber = Math.floor(Math.random() * myEmojis.length);
+  const randomEmoji = myEmojis[randomNumber];
+
+  const numeroAleatorio = Math.floor(Math.random() * myNumbers.length);
+  const randomNumbers = myNumbers[numeroAleatorio];
+
+  const primerDiv = document.getElementById("palo1");
+  primerDiv.innerHTML = randomEmoji;
+
+  const segundoDiv = document.getElementById("palo2");
+  segundoDiv.innerHTML = randomEmoji;
+
+  const numeroDiv = document.getElementById("numero");
+  numeroDiv.innerHTML = randomNumbers;
 };
